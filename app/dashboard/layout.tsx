297798e40a240aaad5cd57c7b78173618d242176
jonathanduran7 <joniduran624@gmail.com>
2025-02-10 21:12:12 -0300

    add layout for dashboard

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <div>
+        <SidebarTrigger />
+        {children}
+      </div>
+    </SidebarProvider>
+  );
+}
