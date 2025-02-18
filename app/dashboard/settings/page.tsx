37877ac84bd4528716856faa3d97db8721bf1af5
jonathanduran7 <joniduran624@gmail.com>
2025-02-18 16:48:52 -0300

    implement usequery in setting page

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,6 +1,23 @@
+"use client";
+import { Currency } from "@/app/models/Currency";
+import { getCurrencies } from "@/app/services/api/currency.api";
 import Accordion from "@/components/ui/accordion";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Page() {
+  const { data, isLoading, error } = useQuery<Currency[]>({
+    queryKey: ["currencies"],
+    queryFn: getCurrencies,
+  });
+
+  if (isLoading) {
+    return <p>Cargando...</p>;
+  }
+
+  if (error) {
+    return <p>Error</p>;
+  }
+
   return (
     <div className="h-full">
       <h1>Settings</h1>
@@ -13,6 +30,12 @@ export default function Page() {
         </Accordion>
         <Accordion title="Monedas">
           <p>Monedas</p>
+
+          <ul>
+            {data?.map((currency) => (
+              <li key={currency.id}>{currency.name}</li>
+            ))}
+          </ul>
         </Accordion>
       </div>
     </div>
