8cb0f13de045d022345de58e4820ba75098e66aa
jonathanduran7 <joniduran624@gmail.com>
2025-02-18 16:07:24 -0300

    feat: implement accordion in setting page

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,3 +1,20 @@
+import Accordion from "@/components/ui/accordion";
+
 export default function Page() {
-  return <div>Setting Page</div>;
+  return (
+    <div className="h-full">
+      <h1>Settings</h1>
+      <div className="w-[80%]">
+        <Accordion title="Cuentas">
+          <p>Cuenta</p>
+        </Accordion>
+        <Accordion title="Categorias">
+          <p>Categorias</p>
+        </Accordion>
+        <Accordion title="Monedas">
+          <p>Monedas</p>
+        </Accordion>
+      </div>
+    </div>
+  );
 }
