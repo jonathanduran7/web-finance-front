285ce23daf219b909288a5e0af237dc7c82eb6db
jonathanduran7 <joniduran624@gmail.com>
2025-02-18 22:06:36 -0300

    feat: remove accordion currency

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,23 +1,8 @@
 "use client";
-import { Currency } from "@/app/models/Currency";
-import { getCurrencies } from "@/app/services/api/currency.api";
 import Accordion from "@/components/ui/accordion";
-import { useQuery } from "@tanstack/react-query";
+import CurrencyAccordion from "./currency-accordion";
 
 export default function Page() {
-  const { data, isLoading, error } = useQuery<Currency[]>({
-    queryKey: ["currencies"],
-    queryFn: getCurrencies,
-  });
-
-  if (isLoading) {
-    return <p>Cargando...</p>;
-  }
-
-  if (error) {
-    return <p>Error</p>;
-  }
-
   return (
     <div className="h-full">
       <h1>Settings</h1>
@@ -29,13 +14,7 @@ export default function Page() {
           <p>Categorias</p>
         </Accordion>
         <Accordion title="Monedas">
-          <p>Monedas</p>
-
-          <ul>
-            {data?.map((currency) => (
-              <li key={currency.id}>{currency.name}</li>
-            ))}
-          </ul>
+          <CurrencyAccordion />
         </Accordion>
       </div>
     </div>
