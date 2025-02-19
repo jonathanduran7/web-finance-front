ff66b6fec51c8b86a280b59eb3d1daf05e73b3eb
jonathanduran7 <joniduran624@gmail.com>
2025-02-19 11:51:41 -0300

    add new modal in factory

--- app/dashboard/settings/category/modal-category-factory.tsx
+++ app/dashboard/settings/category/modal-category-factory.tsx
@@ -1,15 +1,28 @@
 import CategoryModal from "./create-category-modal";
+import CategoryUpdateModal from "./edit-category-modal";
 
 interface ModalFactoryProps {
   type: "create" | "edit" | "none";
-  values?: { id: string; currency: string };
+  values?: { id: string; category: string };
   onClose: () => void;
 }
 
-export default function ModalFactory({ type, onClose }: ModalFactoryProps) {
+export default function ModalFactory({
+  type,
+  onClose,
+  values,
+}: ModalFactoryProps) {
   switch (type) {
     case "create":
       return <CategoryModal isModalOpen={true} setIsModalOpen={onClose} />;
+    case "edit":
+      return values ? (
+        <CategoryUpdateModal
+          isModalOpen={true}
+          setIsModalOpen={onClose}
+          values={values}
+        />
+      ) : null;
     default:
       return null;
   }
