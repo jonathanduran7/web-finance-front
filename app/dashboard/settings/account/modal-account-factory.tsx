9f706cd7e28ae61a785c9c00a4f2ad3e236e1830
jonathanduran7 <joniduran624@gmail.com>
2025-02-19 14:37:15 -0300

    add modal in factory

--- app/dashboard/settings/account/modal-account-factory.tsx
+++ app/dashboard/settings/account/modal-account-factory.tsx
@@ -1,15 +1,33 @@
 import AccountModal from "./create-account-modal";
+import AccountEditModal from "./edit-account-modal";
 
 interface ModalFactoryProps {
   type: "create" | "edit" | "none";
-  values?: { id: string; category: string };
+  values?: {
+    id: string;
+    name: string;
+    currencyId: number;
+    initialBalance: number;
+  };
   onClose: () => void;
 }
 
-export default function ModalFactory({ type, onClose }: ModalFactoryProps) {
+export default function ModalFactory({
+  type,
+  onClose,
+  values,
+}: ModalFactoryProps) {
   switch (type) {
     case "create":
       return <AccountModal isModalOpen={true} setIsModalOpen={onClose} />;
+    case "edit":
+      return values ? (
+        <AccountEditModal
+          isModalOpen={true}
+          setIsModalOpen={onClose}
+          values={values}
+        />
+      ) : null;
     default:
       return null;
   }
