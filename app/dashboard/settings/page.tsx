0df44cb251d1c0a02af88603a629053d19920f01
jonathanduran7 <joniduran624@gmail.com>
2025-02-19 14:42:02 -0300

    add description for page

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -7,8 +7,14 @@ import AccountAccordion from "./account-accordion";
 export default function Page() {
   return (
     <div className="h-full">
-      <h1>Settings</h1>
-      <div className="w-[80%]">
+      <h1 className="text-xl">Settings</h1>
+      <div className="mt-2">
+        <p>
+          Configura tus preferencias de la aplicacion. Puedes modificar tus
+          cuentas, categorias y monedas.
+        </p>
+      </div>
+      <div className="w-[80%] mt-4">
         <Accordion title="Cuentas">
           <AccountAccordion />
         </Accordion>
