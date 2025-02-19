b1060211e9ad72b88d50e913b34f12251ac94b44
jonathanduran7 <joniduran624@gmail.com>
2025-02-19 11:51:09 -0300

    add api for update category

--- app/services/api/category.api.ts
+++ app/services/api/category.api.ts
@@ -29,3 +29,22 @@ export async function removeCategory(id: string): Promise<void> {
     throw new Error("Error deleting category");
   }
 }
+
+export async function updateCategory({
+  categoryName,
+  categoryId,
+}: {
+  categoryName: string;
+  categoryId: string;
+}) {
+  const response = await fetch(`http://localhost:3333/category/${categoryId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name: categoryName }),
+  });
+  if (!response.ok) {
+    throw new Error("Error updating category");
+  }
+}
