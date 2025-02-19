5df9460d17da72a23e07e36cd38fd99d2eab90d4
jonathanduran7 <joniduran624@gmail.com>
2025-02-19 11:04:13 -0300

    create modal for edit currency

---
+++ app/dashboard/settings/edit-currency-modal.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { updateCurrency } from "@/app/services/api/currency.api";
+import Modal from "@/components/ui/modal";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+
+interface CurrencyModalProps {
+  values: {
+    id: string;
+    currency: string;
+  };
+  isModalOpen: boolean;
+  setIsModalOpen: (isOpen: boolean) => void;
+}
+
+export default function CurrencyUpdateModal({
+  isModalOpen,
+  setIsModalOpen,
+  values,
+}: CurrencyModalProps) {
+  const queryClient = useQueryClient();
+  const [currency, setCurrency] = useState(values.currency);
+
+  useEffect(() => {
+    if (isModalOpen) {
+      setCurrency(values.currency);
+    }
+  }, [values.currency, isModalOpen]);
+
+  const { mutate } = useMutation({
+    mutationFn: updateCurrency,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["currencies"] });
+    },
+  });
+
+  const handleSave = () => {
+    mutate({ currencyName: currency, currencyId: values.id });
+    setCurrency("");
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setCurrency("");
+    setIsModalOpen(false);
+  };
+
+  return (
+    <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+      <Modal.Header>Monedas</Modal.Header>
+      <Modal.Body>
+        <p>Edite el nombre</p>
+        <input
+          type="text"
+          className="w-full border border-gray-300 rounded px-4 py-2 mt-2 outline-none"
+          placeholder="Nombre de la moneda"
+          name="currency"
+          value={currency}
+          onChange={(e) => setCurrency(e.target.value)}
+        ></input>
+      </Modal.Body>
+      <Modal.Footer>
+        <div className="flex flex-col mt-6">
+          <button
+            onClick={() => handleSave()}
+            disabled={!currency}
+            className={`mt-4 px-4 py-2 text-white rounded ${!currency ? "bg-gray-400" : "bg-primary"}`}
+          >
+            Editar
+          </button>
+          <button
+            onClick={() => handleCancel()}
+            className="mt-2 px-4 py-2 bg-white text-primary rounded border border-primary"
+          >
+            Cancelar
+          </button>
+        </div>
+      </Modal.Footer>
+    </Modal>
+  );
+}
