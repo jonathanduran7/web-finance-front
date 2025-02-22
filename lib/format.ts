cf883d5a6b77ede0799e6d66ce0fd6331366afdf
jonathanduran7 <joniduran624@gmail.com>
2025-02-22 13:05:37 -0300

    add function for formatcurrency

---
+++ lib/format.ts
@@ -0,0 +1,9 @@
+export const formatCurrency = (value: number): string => {
+  return new Intl.NumberFormat("es-AR", {
+    style: "currency",
+    currency: "ARS",
+    minimumFractionDigits: 2,
+  })
+    .format(value)
+    .replace(/\s/g, "");
+};
