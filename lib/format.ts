dea390bd9979475d3c9e36eac6d1c695ac4a7467
jonathanduran7 <joniduran624@gmail.com>
2025-02-25 12:58:14 -0300

    function por transform date

--- lib/format.ts
+++ lib/format.ts
@@ -7,3 +7,22 @@ export const formatCurrency = (value: number): string => {
     .format(value)
     .replace(/\s/g, "");
 };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const getFormattedDate = (dateInput: any): string => {
+  try {
+    if (typeof dateInput === "string") {
+      const date = new Date(dateInput);
+      if (isNaN(date.getTime())) {
+        return new Date().toISOString().split("T")[0];
+      }
+      return date.toISOString().split("T")[0];
+    } else if (dateInput instanceof Date && !isNaN(dateInput.getTime())) {
+      return dateInput.toISOString().split("T")[0];
+    }
+    return new Date().toISOString().split("T")[0];
+  } catch (error) {
+    console.error("Error formatting date:", error);
+    return new Date().toISOString().split("T")[0];
+  }
+};
