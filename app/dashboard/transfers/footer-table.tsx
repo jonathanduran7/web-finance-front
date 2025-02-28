96c8132220a8a65c17175ae45dd3486c535c7aef
jonathanduran7 <joniduran624@gmail.com>
2025-02-28 13:23:42 -0300

    feat: add footer table component for pagination and data display in transfers

---
+++ app/dashboard/transfers/footer-table.tsx
@@ -0,0 +1,65 @@
+import { PaginatedResponse } from "@/app/interfaces/Response";
+import { Transfer } from "@/app/models/Transfer";
+
+interface Props {
+  data: PaginatedResponse<Transfer>;
+  setPage: (page: number) => void;
+  setLimit: (limit: number) => void;
+}
+
+export default function FooterTable({ data, setPage, setLimit }: Props) {
+  const handlePrevious = (page: number) => {
+    setPage(page);
+  };
+
+  const handleNext = (page: number) => {
+    setPage(page);
+  };
+
+  const handleLimit = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setLimit(parseInt(e.target.value));
+  };
+
+  return (
+    <div className="flex justify-between bg-gray-100 p-4 w-full items-center rounded">
+      <p>
+        Mostrando {data.data.length} de {data.total}
+      </p>
+      <div className="flex gap-4">
+        <div className="flex items-center gap-2">
+          Ver:
+          <select
+            name="limit"
+            className="w-full border border-gray-300 rounded px-4 py-2 outline-none bg-white"
+            onChange={(e) => handleLimit(e)}
+          >
+            <option value={10}>10</option>
+            <option value={20}>20</option>
+            <option value={40}>40</option>
+            <option value={50}>50</option>
+          </select>
+        </div>
+
+        {data.previousPage && (
+          <button
+            className="p-2 bg-gray-200 rounded-lg"
+            onClick={() => handlePrevious(data.previousPage || 1)}
+            disabled={!data.previousPage}
+          >
+            Anterior
+          </button>
+        )}
+
+        {data.nextPage && (
+          <button
+            className="p-2 bg-gray-200 rounded-lg"
+            onClick={() => handleNext(data.nextPage || 1)}
+            disabled={!data.nextPage}
+          >
+            Siguiente
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
