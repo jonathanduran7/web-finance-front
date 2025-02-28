1b4fd9de59fef9d92492a4fc03a901d73aedfe11
jonathanduran7 <joniduran624@gmail.com>
2025-02-28 11:54:39 -0300

    feat: implement transfer page with paginated data fetching and error handling

--- app/dashboard/transfers/page.tsx
+++ app/dashboard/transfers/page.tsx
@@ -1,3 +1,41 @@
+"use client";
+import { getTransferPaginated } from "@/app/services/api/transfer.api";
+import { useQuery } from "@tanstack/react-query";
+
 export default function Page() {
-  return <div>Transfer page</div>;
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["transfers"],
+    queryFn: () => getTransferPaginated({}),
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error...</div>;
+  }
+
+  return (
+    <div>
+      <div>
+        <p className="mb-4 text-3xl">Transferencias</p>
+        <p className="mb-4 text-gray-500 text-sm w-[60%]">
+          Aquí puedes ver todas las transferencias realizadas en tu cuenta.{" "}
+          <br /> Puedes filtrar por fecha, monto, cuenta de origen y cuenta de
+          destino.
+        </p>
+      </div>
+      <div>
+        {data?.data.map((transfer) => (
+          <div key={transfer.id}>
+            <div>{transfer.sourceAccount.name}</div>
+            <div>{transfer.destinationAccount.name}</div>
+            <div>{transfer.amount}</div>
+            <div>{transfer.createdAt}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
