da444405e274dd40144b25ddd6634cc9574a6d5b
jonathanduran7 <joniduran624@gmail.com>
2025-03-03 15:09:01 -0300

    feat: add registration functionality to authentication API

--- app/services/api/auth.api.ts
+++ app/services/api/auth.api.ts
@@ -13,3 +13,23 @@ export async function login(email: string, password: string) {
 
   return response.json();
 }
+
+export async function register(
+  email: string,
+  password: string,
+  confirmPassword: string,
+) {
+  const response = await fetch("http://localhost:3333/auth/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password, confirmPassword }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Error logging in");
+  }
+
+  return response.json();
+}
