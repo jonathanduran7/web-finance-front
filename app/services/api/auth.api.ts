e7ff704c6bca0555c8ebd1c40a373c260487b7be
jonathanduran7 <joniduran624@gmail.com>
2025-03-03 12:12:15 -0300

    feat: add login function to authenticate users via API

---
+++ app/services/api/auth.api.ts
@@ -0,0 +1,15 @@
+export async function login(email: string, password: string) {
+  const response = await fetch("http://localhost:3333/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Error logging in");
+  }
+
+  return response.json();
+}
