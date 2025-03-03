9dd2c442be3f846313778bb69fb5d39474f37141
jonathanduran7 <joniduran624@gmail.com>
2025-03-03 12:12:51 -0300

    feat: add authentication context with login and registration handlers

---
+++ app/context/auth.context.tsx
@@ -0,0 +1,69 @@
+import { createContext, useContext, useState } from "react";
+import { login } from "../services/api/auth.api";
+import { useRouter } from "next/navigation";
+
+interface AuthContextProps {
+  isAuthenticated: boolean;
+  handleLogin: (email: string, password: string) => void;
+  handleRegister: (email: string, password: string) => void;
+  logout: () => void;
+  user: {
+    email: string;
+    id: number;
+  };
+}
+
+const AuthContext = createContext({} as AuthContextProps);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState({
+    email: "",
+    id: 0,
+  });
+  const { push } = useRouter();
+
+  const handleLogin = async (email: string, password: string) => {
+    const response = await login(email, password);
+
+    if (response) {
+      const decrypt = decryptToken(response.access_token);
+      setIsAuthenticated(true);
+      setUser({
+        email: decrypt.email,
+        id: decrypt.sub,
+      });
+      localStorage.setItem("token", JSON.stringify(response));
+      localStorage.setItem(
+        "user",
+        JSON.stringify({
+          email: decrypt.email,
+          id: decrypt.sub,
+        }),
+      );
+      push("/");
+    }
+  };
+
+  const logout = () => {
+    console.log("logout");
+  };
+
+  const handleRegister = (email: string, password: string) => {
+    console.log(email, password);
+  };
+
+  const decryptToken = (token: string) => {
+    return JSON.parse(atob(token.split(".")[1]));
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated, handleRegister, handleLogin, logout, user }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
